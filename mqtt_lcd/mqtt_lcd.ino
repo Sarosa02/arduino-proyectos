d6ec4a5dafe42d60066633d54fd7ebb2d6798f11
Sarosa02 <Saravalero@iesbernatguinovart.com>
2023-11-14 17:35:04 +0100

    comunicar Topic

--- mqtt_lcd/mqtt_lcd.ino
+++ mqtt_lcd/mqtt_lcd.ino
@@ -1,38 +1,42 @@
 /*
- Basic MQTT example
+  Basic MQTT example
 
- This sketch demonstrates the basic capabilities of the library.
- It connects to an MQTT server then:
+  This sketch demonstrates the basic capabilities of the library.
+  It connects to an MQTT server then:
   - publishes "hello world" to the topic "outTopic"
   - subscribes to the topic "inTopic", printing out any messages
     it receives. NB - it assumes the received payloads are strings not binary
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- 
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
+
 */
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-#include <LiquidCrystal_I2C.h>               
+#include <LiquidCrystal_I2C.h>
 
 //Crear el objeto lcd  dirección  0x3F y 16 columnas x 2 filas
-LiquidCrystal_I2C lcd(0x3F,16,2);  
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 // Update these with values suitable for your network.
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 //byte mac[]    = {  0xD8, 0xA7, 0x56, 0x31, 0x06, 0xD0 };
-IPAddress ip(192,168,8,31);
-IPAddress server(192,168,8,11);
+IPAddress ip(192, 168, 8, 31);
+IPAddress server(192, 168, 8, 11);
 
-long ahora,ultimamesura; //variables de tiempo
+long ahora, ultimamesura, segundero; //variables de tiempo
+
+char charTemperature3[]= "00.0";
+
+
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i=0;i<length;i++) {
+  for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
@@ -49,9 +53,9 @@ void reconnect() {
     if (client.connect("arduinoClient")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("outTopic","hello world");
+      client.publish("TopicSara", "hello world");
       // ... and resubscribe
-      client.subscribe("inTopic");
+      client.subscribe("TopicSara");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -72,8 +76,8 @@ void setup()
   lcd.backlight();
   // Escribimos el Mensaje en el LCD.
   lcd.print("HOLA MUNDO");
-  
- 
+
+
 
   client.setServer(server, 1883);
   client.setCallback(callback);
@@ -86,19 +90,23 @@ void setup()
 
 void loop()
 {
-  ahora=millis();
-  if ((ahora-ultimamesura)>3000) {
+ ahora = millis();
+  if ((ahora - ultimamesura) > 3000) {
     ultimamesura = ahora;
+    segundero = ultimamesura / 1000;
     // Ubicamos el cursor en la primera posición(columna:0) de la segunda línea(fila:1)
     lcd.setCursor(0, 1);
-    lcd.print(ultimamesura/1000);
+    lcd.print(segundero);
     lcd.print(" ");
     lcd.print("s");
-    Serial.println(ultimamesura);
+    Serial.println(segundero);
+
+    dtostrf(segundero,4,1, charTemperature3);
+    client.publish("TopicSara", charTemperature3);
   }
-    
- /* if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();*/
+
+   if (!client.connected()) {
+     reconnect();
+    }
+    client.loop();
 }
